82adfc551b46a9afb600a3f67585056f08ce6590
Paul Abesei <abeseipaul@gmail.com>
2026-02-25 18:18:43 +0200

    update

--- mini-proiect.py
+++ mini-proiect.py
@@ -57,9 +57,11 @@ Pentru persoanele care depasesc termenul limita se vor scadea cate 0.25p pentru 
 Maximul de zile de intarziere este de 14 zile, dupa care proiectul nu va mai fi acceptat, iar nota va fi 1.
 """
 
+import json
 persoane = [{"CNP":"1960101123456","Nume":"Popescu","Prenume":"Andrei","Varsta":29,"Salar":4500,"Departament":"IT","Senioritate":"mid"},{"CNP":"2950502234567","Nume":"Ionescu","Prenume":"Maria","Varsta":30,"Salar":5200,"Departament":"HR","Senioritate":"senior"},{"CNP":"1980703345678","Nume":"Georgescu","Prenume":"Mihai","Varsta":27,"Salar":4000,"Departament":"Marketing","Senioritate":"mid"},{"CNP":"6020814456789","Nume":"Dumitrescu","Prenume":"Elena","Varsta":24,"Salar":3500,"Departament":"Financiar","Senioritate":"junior"},{"CNP":"1970905567890","Nume":"Popa","Prenume":"Alexandru","Varsta":33,"Salar":6000,"Departament":"IT","Senioritate":"senior"},{"CNP":"2961016678901","Nume":"Stan","Prenume":"Ioana","Varsta":28,"Salar":4700,"Departament":"Vanzari","Senioritate":"mid"},{"CNP":"1951127789012","Nume":"Marin","Prenume":"Cristian","Varsta":31,"Salar":5500,"Departament":"Logistica","Senioritate":"senior"},{"CNP":"2971208890123","Nume":"Voicu","Prenume":"Alina","Varsta":26,"Salar":3800,"Departament":"IT","Senioritate":"junior"},{"CNP":"1980112991234","Nume":"Morar","Prenume":"Daniel","Varsta":35,"Salar":6200,"Departament":"Management","Senioritate":"senior"},{"CNP":"2990213102345","Nume":"Radu","Prenume":"Bianca","Varsta":23,"Salar":3400,"Departament":"Marketing","Senioritate":"junior"},{"CNP":"1970314213456","Nume":"Barbu","Prenume":"Paul","Varsta":32,"Salar":5800,"Departament":"Financiar","Senioritate":"senior"},{"CNP":"2960415324567","Nume":"Tudor","Prenume":"Raluca","Varsta":27,"Salar":4200,"Departament":"HR","Senioritate":"mid"},{"CNP":"1980516435678","Nume":"Neagu","Prenume":"Vlad","Varsta":30,"Salar":5100,"Departament":"IT","Senioritate":"mid"},{"CNP":"2990617546789","Nume":"Florea","Prenume":"Diana","Varsta":25,"Salar":3600,"Departament":"Vanzari","Senioritate":"junior"},{"CNP":"1960718657890","Nume":"Preda","Prenume":"Sorin","Varsta":34,"Salar":6100,"Departament":"Logistica","Senioritate":"senior"},{"CNP":"2970819768901","Nume":"Enache","Prenume":"Laura","Varsta":26,"Salar":3900,"Departament":"Financiar","Senioritate":"junior"},{"CNP":"1950920879012","Nume":"Ilie","Prenume":"Gabriel","Varsta":36,"Salar":6500,"Departament":"Management","Senioritate":"senior"},{"CNP":"2981021980123","Nume":"Mihalache","Prenume":"Adina","Varsta":28,"Salar":4400,"Departament":"Marketing","Senioritate":"mid"},{"CNP":"1971122091234","Nume":"Sandu","Prenume":"Robert","Varsta":31,"Salar":5700,"Departament":"IT","Senioritate":"senior"},{"CNP":"2991223102345","Nume":"Lazar","Prenume":"Monica","Varsta":24,"Salar":3300,"Departament":"HR","Senioritate":"junior"}]
+# persoane = []
 
-
+#codul merge - scrie persoane in lista ca si dictionare
 def adauga_persoane ():
     cnp = input("CNP :")
     nume = input("Nume : ")
@@ -78,77 +80,74 @@ def adauga_persoane ():
         "senioritate" : senioritate
     }
     persoane.append(persoana)
+    with open ('lista_angajati.json','w') as my_file :
+        json.dump(persoane,my_file, indent = 4)
 
-# def calculeaza_media(nota_romana, nota_mate, nota_engl):
-#     x = round((nota_romana + nota_mate + nota_engl)/3,2)
-#     return x
+## codul merge - stie sa caute si sa afiseze persoana cautata            
+def cauta_persoane (cauta):
+    with open ('lista_angajati.json','r') as my_file :
+        date = json.load(my_file)
+    for elem in date :
+        if elem['CNP'] == cauta :
+            print(elem)
+        else :
+            print(f'Nu s-a gasit persoana cu CNP : {cauta}')
+            break
 
-# def ia_media(elev):
-#     return elev['media']
+# codul merge - stie sa modifice fiecare informatie al angajatului daca se doreste
+def modificare_persoane (cauta):
+    cauta = input("introdu cnp cautat pentru modificare")
+    with open ('lista_angajati.json','r') as my_file :
+        date = json.load(my_file)
+    for elem in date : 
+        if elem['CNP'] == cauta :
+            while True : 
+                optiune = input('Ce anume vrei sa modifici : Nume, Prenume, Varsta, Salar, Departament, Senioritate ? sau exit pentru iesire si salvare')
+                if optiune == 'exit':
+                    break
+                if optiune.capitalize() == 'Nume':
+                    var = input("Intordu noul nume : ")
+                    elem['Nume'] = var.capitalize()
+                if optiune.capitalize() == 'Prenume':
+                    var = input("Intordu noul prenume : ")
+                    elem['Prenume'] = var.capitalize()
+                if optiune.capitalize() == 'Varsta':
+                    while True : 
+                        var = input("Intordu noua varsta : ")
+                        if var.isdigit() :
+                            elem['Varsta'] = int(var)
+                            print('Varsta a fost modificata !') 
+                            break                       
+                        else :
+                            print('Varsta trebuie sa fie formata din cifre')                            
+                if optiune.capitalize() == 'Salar' :
+                    while True :
+                        var = float(input("Intordu noul salariu : "))
+                        if var.isdigit() :
+                            elem['Salar'] = var
+                            print("Salariul a fost modificat !")
+                            break
+                        else :
+                            print("Salariul trebuie sa fie format din cifre")
+                if optiune.capitalize() == 'Departament':
+                    var = input("Intordu noul departament : ")
+                    elem['Departament'] = var.capitalize()
+                if optiune.capitalize() == 'Senioritate':
+                    var = input("Intordu noua senioritate : ")
+                    elem['Senioritate'] = var.capitalize()
+    with open ('lista_angajati.json','w') as my_file :
+            json.dump(date,my_file, indent = 4)
 
-# def ia_nume(elev):
-#     return elev['nume']
+def stergere_persoane(cauta):
+    with open ('lista_angajati.json','r') as my_file :
+        date = json.load(my_file)
+    for elem in date : 
+        if elem['CNP'] == cauta :
+            elem.clear()
+    with open ('lista_angajati.json','w') as my_file :
+            json.dump(date,my_file, indent = 4)
 
-def afiseaza_angajati():
-    with open ("angajati.json",'r') as my_file :
-    for elev in elevi :
-        print(f"{elev['nume']} {elev['prenume']} | "
-            f"Romana: {elev['nota romana']} | "
-            f"Matematica: {elev['nota mate']} | "
-            f"Engleza: {elev['nota engleza']} | "
-            f"Media: {elev['media']}")
-        
-# def afisare_alfabetic():
-#     elevi_sortati = sorted(elevi, key=ia_nume)
-#     for elev in elevi_sortati:
-#         print(f"{elev['nume']} {elev['prenume']}")
-        
-# def sterge_elevi():
-#     nume = input("Ce nume vrei elimini ? ")
-#     prenume = input("Ce prenume vrei sa elimini ?")
-#     for elev in elevi :
-#         if elev["nume"] == nume and elev["prenume"] == prenume:
-#             elevi.remove(elev)
-#             print("Am sters")
-#             return
-#     print("Elevul nu a fost gasit")
 
-# def modificare ():
-#     nume = input("Ce nume sa modific : ")
-#     prenume = input("Ce prenume sa modific : ")
-#     for elev in elevi: 
-#         if elev['nume'] == nume and elev['prenume'] == prenume:
-#             elev['nota romana'] = float(input("Nota roamana noua: "))
-#             elev['nota mate'] = float(input("Nota mate noua: "))
-#             elev['nota engleza'] = float(input("Nota engleza noua: "))
-#             elev['media'] = calculeaza_media(elev['nota romana'], elev['nota mate'], elev['nota engleza'])
-#             print("Date modificate success!")
-#             return
-#     print("Elev negasit!")
-
-# def cauta_elevi():
-#     nume = input("Ce nume vrei ? ")
-#     prenume = input("Ce prenume vrei ?")
-#     for elev in elevi :
-#         if elev["nume"] == nume and elev["prenume"] == prenume:
-#             print(f"{elev['nume']} {elev['prenume']} | "
-#             f"Romana: {elev['nota romana']} | "
-#             f"Matematica: {elev['nota mate']} | "
-#             f"Engleza: {elev['nota engleza']} | "
-#             f"Media: {elev['media']}")
-#             return
-#     print("Elevul nu a fost gasit")
-
-# def afiseaza_media_crescator():
-#     elevi_sortati = sorted(elevi, key=ia_media)
-#     for elev in elevi_sortati:
-#         print(f"{elev['nume']} {elev['prenume']} | Media: {elev['media']}")
-
-# def medie_5 ():
-#     for elev in elevi :
-#         if elev['media'] >= 5:
-#             print(f"{elev['nume']} {elev['prenume']} | Media: {elev['media']}")
-            
 while True :
     print(
     """
@@ -167,24 +166,27 @@ while True :
         ) 
     optiune = input("Alege optiune : ")
     if optiune == '0' :
-        print("Ai iesit din sistem")
+        print("Ai iesit din sistem ! ")
         break
     if optiune == "1" :
         adauga_persoane()
     if optiune == "2" :
-        # afiseaza_elevi()
-        afiseaza_elevi(elevi)
+        cauta = input("Introdu cnp-ul cautat : ")
+        if len(cauta) == 13 :
+            cauta_persoane (cauta)
+        else : 
+            print(f'CNP-ul : {cauta} nu are toate cifrele')
     if optiune == "3" :
-        modificare()
-    if optiune == "4":
-        sterge_elevi()
-    if optiune == "5":
-        cauta_elevi()
-    if optiune == "6":
-        # afiseaza_media_crescator()
-        afiseaza_elevi( elevi, sort = True, field = ia_media )
-    if optiune == "7":
-        medie_5()
-    if optiune == "8":
-        # afiseaza_alfabetic()
-        afiseaza_elevi( elevi, sort = True, field = ia_nume )
+        cauta = input("Introdu cnp-ul persoanei unde vrei sa faci modificari : ")
+        if len(int(cauta)) == 13 :
+            modificare_persoane (cauta)
+            print(f'''
+                  Modificarile au fost salvate !
+                  Acestea sunt noile date ale persoanei :
+                  {cauta_persoane(cauta)}
+                  ''')
+        else : 
+            print(f'CNP-ul : {cauta} nu are toate cifrele')
+    if optiune == '4' :
+        cauta = input("Intordu cnp-ul persoanei pe care vrei sa o stergi : ")
+        stergere_persoane(cauta)
